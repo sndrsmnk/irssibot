9bdb5ee23b0f81a0582737fcd49d192fe866f03c
Sander Smeenk <github@freshdot.net>
2014-03-15 16:16:06 +0100

    add new layer of 420 to streak logic

--- irssi/irssibot/modules/fipo.pl
+++ irssi/irssibot/modules/fipo.pl
@@ -2,13 +2,73 @@
 # CMDS fipo
 # CMDS fipostats
 # CMDS fiporeset
+# CMDS fiposet
 return if (not perms("user"));
 
 my $msg = $$irc_event{msg};
 return if $msg !~ $$state{bot_triggerre};
 $msg =~ s#$$state{bot_triggerre}##;
 
-if ($msg =~ /^fipo\s*$/) {
+$$irc_event{channel} = "#cistron" if $$irc_event{channel} eq "#irssibot";
+
+if ($msg =~ /^fipostats(?:\s+.*)?/) {
+    my $oldnick = my $streaknick = ""; my @winningstreaknick = ();
+    my $streak = my $winningstreak = 1; my $nick_stats = {};
+    foreach my $day (sort keys %{$$state{__fipo}{$$irc_event{channel}}}) {
+        my $nick = $$state{__fipo}{$$irc_event{channel}}{$day};
+        $$nick_stats{$nick}++;
+
+        if ($nick eq $oldnick) {
+            $streak++;
+            $streaknick = $nick;
+        } else {
+            if ($streak > $winningstreak) {
+                $winningstreak = $streak;
+                @winningstreaknick = ( $streaknick );
+            } elsif ($streak == $winningstreak) {
+                push @winningstreaknick, $streaknick unless grep { /^$streaknick$/ } @winningstreaknick;
+            }
+            $streak = 1;
+            $streaknick = $nick;
+            $oldnick = $nick;
+        }
+    }
+    say("Longest streak of $winningstreak day(s) by " . join(", ", @winningstreaknick) . "!");
+    
+    my $count = 1;
+    my $msg = undef;
+    foreach my $nick (
+        reverse sort { $$nick_stats{$a} <=> $$nick_stats{$b} }
+            keys %$nick_stats) {
+
+        # prevent hilights
+        my @letters = split(//, $nick);
+        my $out_nick = shift @letters;
+        $out_nick .= "\0030\003";
+        $out_nick .= join("", @letters);
+
+        $msg .= ($msg?", $out_nick($$nick_stats{$nick})":"$out_nick($$nick_stats{$nick})");
+        last if $count++ >= 5;
+    }
+    $count--; # meh.
+
+    return say("Top $count FIPO'ers: $msg");
+    
+
+} elsif ($msg =~ /^fiporeset\s*$/) {
+    return reply("no permission!") if not perms("owner");
+    $$state{__fipo}{$$irc_event{channel}} = {};
+    return say("Fipo stats for $$irc_event{channel} were reset.");
+
+
+} elsif ($msg =~ /^fiposet\s*([^\s]+)\s(.*)$/) {
+    return reply("no permission!") if not perms("owner");
+    my $date = $1; my $nick = $2;
+    $$state{__fipo}{$$irc_event{channel}}{$date} = $nick;
+    save_configuration();
+    return say("fipo for $date set to $nick!");
+
+} elsif ($msg =~ /^fipo(?:\s+.*)?/) {
     my @lt = localtime(time);
     my $day = sprintf("%4d%02d%02d", $lt[5]+1900, $lt[4]+1, $lt[3]);
 
@@ -24,71 +84,5 @@ if ($msg =~ /^fipo\s*$/) {
         return reply("Yes! :D  It was YOU!  YOU SCORED TODAY'S FIPO!!  \\o/")
             if ($$irc_event{nick} eq $fipo_nick);
     }
-
-
-} elsif ($msg =~ /^fipostats\s*$/) {
-    my $nick_stats = {};
-    my $old_nick = undef;
-    my $streak_counter = 1;
-
-    my $day_nick = "";
-    foreach my $day (sort keys %{$$state{__fipo}{$$irc_event{channel}}}) {
-        $day_nick = $$state{__fipo}{$$irc_event{channel}}{$day};
-
-        if ($day_nick ne $old_nick) {
-            if ($streak_counter > $$nick_stats{streaker}{count}) {
-                $$nick_stats{streaker}{nick} = defined $old_nick?$old_nick:$day_nick;
-                $$nick_stats{streaker}{count} = $streak_counter;
-
-            } elsif (
-                    ($streak_counter == $$nick_stats{streaker}{count})
-                    and ($$nick_stats{streaker}{nick} ne "")
-                    and ($$nick_stats{streaker}{nick} !~ m#\W$daynick\W#)) {
-
-                $$nick_stats{streaker}{nick} = $$nick_stats{streaker}{nick} . " and " . $day_nick;
-                $$nick_stats{streaker}{count} = $streak_counter;
-            }
-
-            $old_nick = $day_nick;
-            $streak_counter = 1;
-            $$nick_stats{$day_nick}++;
-        } else {
-            $streak_counter++;
-            $$nick_stats{$day_nick}++;
-        }
-    }
-    if ($streak_counter > $$nick_stats{streaker}{count}) {
-        $$nick_stats{streaker}{nick} = $day_nick;
-        $$nick_stats{streaker}{count} = $streak_counter;
-    }
-
-    say("Longest streak of $$nick_stats{streaker}{count} day(s) by $$nick_stats{streaker}{nick}!");
-    delete $$nick_stats{streaker};
-    
-    my $count = 1;
-    my $msg = undef;
-    foreach my $nick (
-        reverse sort { $$nick_stats{$a} <=> $$nick_stats{$b} }
-            keys %$nick_stats) {
-
-        # prevent hilights
-        my @letters = split(//, $nick);
-        my $out_nick = shift @letters;
-        $out_nick .= "\0030\003";
-        $out_nick .= join("", @letters);
-
-        $msg .= ($msg?", $out_nick($$nick_stats{$nick})":"$out_nick($$nick_stats{$nick})");
-        last if $count++ > 5;
-    }
-    $count--; # meh.
-
-    return say("Top $count FIPO'ers: $msg");
-    
-
-} elsif ($msg =~ /^fiporeset\s*$/) {
-    return reply("no permission!") if not perms("owner");
-    $$state{__fipo}{$$irc_event{channel}} = {};
-    return say("Fipo stats for $$irc_event{channel} were reset.");
-
 
 }
