65cc983b5244c53bd89b9726db303f99a81f656d
Sander Smeenk <github@freshdot.net>
2014-04-09 23:01:52 +0200

    improvements, save bug(?)

--- irssi/irssibot/modules/save.pl
+++ irssi/irssibot/modules/save.pl
@@ -5,6 +5,20 @@ return reply("you lack permission.") if (not perms("admin"));
 
 save_configuration();
 
+my $temp = {};
+foreach my $key (keys %$state) {
+    # XXX not nice. use prefix?
+    next if $key =~ m#^(?:act_channel|user_info|dbh|bot_is_op|modules)$#;
+    $$temp{$key} = $$state{$key};
+}
+
+open (FD, ">$$state{bot_configfile}") or msg("Problems while writing configuration file: $!\n");
+print FD Dumper($temp);
+close(FD);
+$temp = {}; undef $temp;
+msg("Irssibot configuration file was saved.");
+
 my @stat = stat($$state{bot_configfile});
 say("Configuration is " . $stat[7] . " bytes, last modified " . localtime($stat[9]));
 say("Done.");
+
--- irssi/irssibot/modules/whois.pl
+++ irssi/irssibot/modules/whois.pl
@@ -22,9 +22,9 @@ foreach my $channel (Irssi::channels()) {
 
 my $out = "$whois_nick is ";
 if ($whois_host eq "") {
-    $out .= "not on channel $$irc_event{target}. ";
+    $out .= "not on channel $$irc_event{target} ";
 } else {
-    $out .= "on channel $$irc_event{target} as $whois_host. ";
+    $out .= "on channel $$irc_event{target} as $whois_host ";
 }
 
 
@@ -33,27 +33,25 @@ my $user_info;
 if ($whois_host) {
     $user_info = $$state{dbh}->selectrow_hashref("select u.* from ib_users u, ib_hostmasks h WHERE u.id = h.users_id AND h.hostmask = ?", undef, $whois_host);
     if (exists $$user_info{ircnick}) {
-        $out .= "$whois_nick is recognised as $$user_info{ircnick}. ";
+        $out .= "and is recognised as user $$user_info{ircnick}. ";
         $recognized++;
     } else {
-        $out .= "$whois_nick is not recognised as a registered user. ";
+        $out .= "and is not recognised as a registered user ";
     }
 }
 
-
-if (not $recognized) {
-    $user_info = $$state{dbh}->selectrow_hashref("SELECT * FROM ib_users WHERE ircnick = ?", undef, $whois_nick);
-    if (exists $$user_info{ircnick}) {
-        $out .= "A registered user named $whois_nick was found. ";
-    } else {
-        return say($out . "No registered user named $whois_nick was found either.");
-    }
+$user_info = $$state{dbh}->selectrow_hashref("SELECT * FROM ib_users WHERE ircnick = ?", undef, $whois_nick);
+if (exists $$user_info{ircnick}) {
+    $out .= "but a registered user named $whois_nick was found. ";
+} else {
+    return say($out . "and no registered user named $whois_nick was found either.");
 }
 
+say($out);
+$out = "";
+return if not exists $$user_info{id};
 
 $out .= "Permissions: ";
-
-
 my $users_id = $$user_info{id};
 my $sth = $$state{dbh}->prepare("SELECT * FROM ib_perms WHERE users_id = ? AND (channel = ? OR channel = '') ORDER BY channel ASC");
 $sth->execute($users_id, $$irc_event{channel});
@@ -67,5 +65,16 @@ while (my $row = $sth->fetchrow_hashref()) {
 }
 $sth->finish();
 
+say($out);
+$out = "";
 
+$out .= "Hostmasks: ";
+my $sth = $$state{dbh}->prepare("SELECT * FROM ib_hostmasks WHERE users_id = ? ORDER BY hostmask ASC");
+$sth->execute($users_id);
+while (my $row = $sth->fetchrow_hashref()) {
+    $out .= $$row{hostmask} . ", ";
+}
+$sth->finish();
+
+$out =~ s/, $//;
 say($out);
