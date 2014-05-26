9cbc5fd1586debb5bd7e7c3bf28ae26583f90611
Sander Smeenk <github@freshdot.net>
2014-05-26 19:56:41 +0200

    clean up this act

--- irssi/irssibot/modules/wolfram.pl
+++ irssi/irssibot/modules/wolfram.pl
@@ -8,6 +8,8 @@ use URI::Escape;
 use XML::XPath;
 use XML::XPath::XMLParser;
 
+#
+# Admin can /msg the bot '!set wolfrap_app_id IDHERE', handle that:
 if (($$irc_event{irc_event} eq "message_private") and ($$irc_event{target} eq $$irc_event{channel})) {
     if ($$irc_event{msg} =~ m#^!set\s+wolfram_app_id\s+(.*)#) {
         return say("You no admin. Go away.") if not perms('admin');
@@ -20,15 +22,13 @@ if (($$irc_event{irc_event} eq "message_private") and ($$irc_event{target} eq $$
 
 my $wolfram_app_id = "";
 $wolfram_app_id = (exists $$state{__wolfram}{appid} ? $$state{__wolfram}{appid} : "");
-return reply("This plugin is not configured yet.") if $wolfram_app_id eq "";
+return reply("the bot owner should configure the Wolfram API ID first.") if $wolfram_app_id eq "";
 
-my $msg = $$irc_event{msg};
 my $args = $$irc_event{args};
-my $channel = $$irc_event{channel};
 
 my $url = "http://api.wolframalpha.com/v2/query?appid=$wolfram_app_id&input=".uri_escape($args);
 my $content = fetchURL($url);
-return reply("The query to Wolfram failed.") if not defined $content;
+return reply("the query to Wolfram failed.") if not defined $content;
 
 my $query_input = my $query_decapprox = my $query_result = "";
 
@@ -37,17 +37,15 @@ my $nodeset = $xp->find("//pod[\@id='Input']/subpod/plaintext/text()");
 my ($node) = $nodeset->get_nodelist;
 if (defined $node) { $query_input = XML::XPath::XMLParser::as_string($node); }
 
-my $nodeset = $xp->find("//pod[\@id='DecimalApproximation']/subpod/plaintext/text()"); #/queryresult/pod[\@title='DecimalApproximation']/subpod/plaintext/text()");
-my ($node) = $nodeset->get_nodelist;
-if (defined $node) { $query_decapprox = XML::XPath::XMLParser::as_string($node); }
-
 my $nodeset = $xp->find("//pod[\@id='Result']/subpod/plaintext/text()");
 my ($node) = $nodeset->get_nodelist;
 if (defined $node) { $query_result = XML::XPath::XMLParser::as_string($node); }
 
-say("Input Interpretation: " . $query_input);
-say("Decimal aproximation: " . $query_decapprox) if (defined $query_decapprox and ($query_decapprox ne ""));
-say("Result: " . $query_result);
+if ( ($query_input =~ m#^\s*$#) or ($query_result =~ m#^\s*$#) ) {
+    return reply("query not understood.");
+} else {
+    return reply($query_input . " = " . $query_result;
+}
 
 return;
 
