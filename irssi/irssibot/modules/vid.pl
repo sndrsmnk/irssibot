39407db068fabf40e05342e9a4f1cd0dd21b3b1e
Sander Smeenk <github@freshdot.net>
2015-03-28 20:35:16 +0100

    new: vid info, small fixes

---
+++ irssi/irssibot/modules/vid.pl
@@ -0,0 +1,170 @@
+#!/usr/bin/perl -w
+# CMDS vid
+#
+# Parses VID.nl's 'overzicht' page!
+#   http://www.vid.nl/VI/overzicht
+#
+# !vid
+# !vid A12
+# !vid -a
+#
+use LWP::UserAgent;
+
+
+return if (not perms("user"));
+
+
+my $vidContent = "";
+my $vid_overzicht = "[geen info, parser fout]";
+
+$vidContent = fetchURL("http://www.vid.nl/VI/overzicht");
+if ($vidContent =~ m#<div.+?overzicht-aantal">(.+?)</div>#) {
+    $vid_overzicht = $1;
+}
+$vidContent =~ s#^.*(<div id="overzicht-verkeer".*</div>).+?<div id="overzicht-ov".*#$1#s;
+my @vidInfo = parse_vidContent($vidContent);
+
+
+if ($$irc_event{cmd} eq 'vid' and $$irc_event{args} =~ m#(?:\-a|[a-z]\d+)#i) {
+    say("$vid_overzicht:") if ($$irc_event{args} eq '-a');
+    foreach my $elem (@vidInfo) {
+        next if (lc($$elem{wegnr}) ne lc($$irc_event{args}) and lc($$irc_event{args}) ne "-a");
+
+        my $txt = $$elem{wegnr} . ($$elem{hoofdtraject}ne""?" ".$$elem{hoofdtraject}:"") ." ". $$elem{traject};
+        if (defined $$elem{lengte}) {
+            $txt .= ", lengte " . $$elem{lengte} . " (" . $$elem{file_staat} . ")"
+        }
+        $txt .= ": " . $$elem{bericht};
+
+        say($txt);
+    }
+
+
+} elsif ($$irc_event{cmd} eq 'vid' and $$irc_event{args} eq "") {
+    my $wegen = {};
+    foreach my $elem (@vidInfo) {
+        $$wegen{$$elem{wegnr}}{aantal}++;
+        if (defined $$elem{lengte}) {
+            $$elem{lengte} =~ tr/a-z//d;
+            $$wegen{$$elem{wegnr}}{lengte} += $$elem{lengte};
+        }
+    }
+
+    my $txt = "";
+    foreach my $wegnr (sort keys %$wegen) {
+        $txt .= "$wegnr (" . $$wegen{$wegnr}{aantal}; # . " " . ($$wegen{$wegnr}{aantal}>1?"meldingen":"melding");
+        if (defined $$wegen{$wegnr}{lengte}) {
+            $txt .= ", file ".$$wegen{$wegnr}{lengte}."km";
+        }
+        $txt .= "), ";
+    }
+    $txt =~ s#,\s+$##;
+    
+    say ("$vid_overzicht: $txt");
+}
+
+
+
+#
+#
+#
+#
+
+
+
+sub parse_vidContent {
+    my ($vidContent) = @_;
+
+    my @ret_array = ();
+
+    my $prev_wegnr = my $prev_hoofdtraject = "";
+    while ($vidContent =~ m#<dl>(.+?)</dl>#gs) {
+        my $fileContent = $1;
+
+        my $vid = {};
+        if ($fileContent =~ m#"vi-hoofdtraject.+?vi-wegnr">(.+?)</span>(.+?)</dt>#s) {
+            $$vid{wegnr} = $1;
+            $$vid{hoofdtraject} = $2;
+            $prev_wegnr = $$vid{wegnr};
+            $prev_hoofdtraject = $$vid{hoofdtraject};
+        } else {
+            $$vid{wegnr} = $prev_wegnr;
+            $$vid{hoofdtraject} = '';
+        }
+
+        if ($fileContent =~ m#"vi-traject.+?">(.+?)</dd>#s) {
+            $$vid{traject} = $1;
+            $$vid{traject} =~ s#<a href=.+?</a>##s; # strip camera url
+            $$vid{traject} =~ s#<.+?>##g; 
+
+        } else {
+            $$vid{traject} = '[parsefout]';
+        }
+
+        if ($fileContent =~ m#"vi-(bericht|langdurig).+?">(.*)</dd>#s) {
+            $$vid{soort} = $1;
+            my $berichtContent = $2;
+
+            if ($berichtContent =~ s#.+?"vid-sprite vs-file-(\w+)">\s+</span>##s) {
+                my $state = $1;
+                $$vid{file_staat} = $state;
+                $$vid{file_staat} = 'geen' if ($state eq "leeg");
+                $$vid{file_staat} = 'ongeval' if ($state eq "excl");
+                $$vid{file_staat} = 'afnemend' if ($state eq "down");
+                $$vid{file_staat} = 'toenemend' if ($state eq "up");
+                $$vid{file_staat} = 'gelijkblijvend' if ($state eq "same");
+            } else {
+                $$vid{file_staat} = '[parsefout]';
+            }
+
+            if ($berichtContent =~ s#.+?"vi-km.+?>(.+?)</span>##s) {
+                $$vid{lengte} = $1;
+            } else {
+                $$vid{lengte} = undef;
+            }
+
+            $berichtContent =~ s#<.+?>##g;
+            $berichtContent =~ s#\r?\n##g;
+            $berichtContent =~ s#\s{2,}# #g;
+            $berichtContent =~ s#^\s+##g;
+            $berichtContent =~ s#\s+$##g;
+            $berichtContent =~ s#omleiding ingesteld#, omleiding ingesteld.#;
+            $$vid{bericht} = $berichtContent;
+
+        } else {
+            $$vid{soort} = '[parsefout]';
+        }
+
+        push @ret_array, $vid;
+    }
+
+    return @ret_array;
+}
+
+
+sub fetchURL {
+    my ($url) = @_;
+    my $ret = "";
+
+    # URLs must be absolute
+    $url = "http://" . $url if ($url !~ m#^https?://#);
+
+    my $lwp = LWP::UserAgent->new;
+    $lwp->max_redirect(7);
+    $lwp->requests_redirectable(['GET', 'HEAD']);
+    $lwp->timeout(15);
+    $lwp->agent('Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0');
+    my $req = HTTP::Request->new(GET => $url);
+    my $res = $lwp->request($req);
+    if (!$res->is_success) {
+        msg("GET failed on '$url'");
+        msg("---- " . $res->status_line);
+
+    } else {
+        $ret = $res->content;
+
+    }
+
+    return $ret;
+}
+
